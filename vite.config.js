fcff0654273de91a8e9f299687473f153029d585
Schepie <geert.schepers@gmail.com>
2026-02-13 16:05:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,15 +19,15 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'logo.jpg',
+            src: 'app-icon-round.png',
             sizes: '192x192',
-            type: 'image/jpeg',
+            type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: 'logo.jpg',
+            src: 'app-icon-round.png',
             sizes: '512x512',
-            type: 'image/jpeg',
+            type: 'image/png',
             purpose: 'any maskable'
           }
         ]
